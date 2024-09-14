703542896fc7768c7f982f76f1637e0b4d4e28c1
HoiYau <126309552+HoiYau@users.noreply.github.com>
2024-09-14 20:57:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -373,7 +373,7 @@ elif selection == 'Input data':
                 color=user_color,
                 fill=True,
                 fill_color=user_color,
-                icon=folium.Icon(icon='user', color='user_color'),
+                icon=folium.Icon(icon='user', color=user_color),
                 popup=f'User Input<br>Cluster: {user_group}<br>Latitude: {user_org_df["latitude"].values[0]}<br>Longitude: {user_org_df["longitude"].values[0]}<br>Feedback: {user_org_df["Feedback"].values[0]}<br>Output: {user_org_df["Output"].values[0]}'
             ).add_to(cluster_groups[user_group])
 
